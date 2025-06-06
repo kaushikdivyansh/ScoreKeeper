c95e0f57196dee8730050c40b307c81fe680513d
Divyansh Kaushik <kaushik.divyansh@icloud.com>
2025-06-06 09:16:39 -0500

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Divyansh Kaushik on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
